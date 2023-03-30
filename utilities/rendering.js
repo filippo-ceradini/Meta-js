99087a646a9af70e902521dbe1d3fea4d9be101a
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-03-30 23:00:22 +0200

    Added fix to display added posts

--- utilities/rendering.js
+++ utilities/rendering.js
@@ -1,28 +1,21 @@
 import fs from "fs";
 
-
-
 function renderPage(page, cssString, tabTitle) {
     page = readPage(page)
     let pageLinks = fs.readdirSync("./public/pages/NewPages/");
     const stringLinks = pageLinks.map(function(page) {
         return `<a class="dropdown-item" href="/NewPages/${page}">${page.replace(".html","")}</a>`;
     });
-    console.log(stringLinks)
     let navbar = fs.readFileSync("./public/components/navbar.html").toString()
         .replace("$CSS_FILENAME", cssString)
         .replace("$TAB_TITLE", tabTitle)
     if (stringLinks.length > 0) {
         navbar = navbar.replace("$POSTS_ENTRIES", stringLinks.join(""));
     } else {navbar = navbar.replace("$DISPLAY_POSTS", "d-none")}
-
-
-    console.log(stringLinks)
     const footer = fs.readFileSync("./public/components/footer.html").toString()
         .replace("$FOOTER_YEAR", `© ${new Date().getFullYear()}`);
     return navbar + page + footer;
 }
-
 
 
 function readPage(pagePath) {
